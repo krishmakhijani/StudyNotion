d54a81765f4ee18caacaac58039d8d6869aefeba
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2024-06-28 00:42:42 +0530

    controller/AUTH

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,11 +1,108 @@
-
-
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator")
 
 //sendOTP
+exports.sendOTP = async (req, res) => {
 
-//signUp
+    try{
+        const {email} = req.body
 
-//Login
+        const checkUserPresent = User.findOne({email});
 
-//changePassword
+        if(checkUserPresent){
+            return res.status(401).json({
+                success : false,
+                message : "User already present"
+            })
+        }
 
+        var otpgeneric = otpGenerator.generate(6,{
+            upperCaseAlphabets :false,
+            lowerCaseAlphabets : false,
+            specialChars : false
+        })
+        console.log("OTP Generated : ",otp)
+        
+        const result = await OTP.findOne({otp:otp})
+
+        while(result){
+            otpgeneric = otpGenerator.generate(6,{
+                upperCaseAlphabets :false,
+                lowerCaseAlphabets : false,
+                specialChars : false
+            })
+            const result = await OTP.findOne({otp:otpgeneric}) 
+        }
+
+        const otpPayload = {email,otpgeneric}
+
+        const otpBody = await OTP.create(otpPayload)
+        console.log(otpBody)
+
+        res.status(200).json({
+            success : true,
+            message : 'OTP Sent Successfully'
+        })
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).json({
+            success:false,
+            message: err.message
+        })
+
+    }
+};
+
+//SignUp
+
+exports.signUp = async (req,res) => {
+    const{
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+        accountType,
+        contactNumber,
+        otp
+    } = req.body;
+
+    if(!firstName || !lastName || email || !password || !confirmPassword || otp) {
+        return res.status(403).json({
+            success: false,
+            message: "All fields are required",
+        })
+    }
+
+    if(password != confirmPassword){
+        return res.status(500).json({
+            success:false,
+            message : "Password doesnot match "
+        })
+    }
+
+    const exsistingUser = User.findOne({email})
+    if(exsistingUser){
+        return res.status(500).json({
+            success : false,
+            message : "User already exsists"
+        })
+    }
+
+    const recentOTP = await OTP.find({email}).sort({createdAt:-1}.limit(1)) //understand
+    console.log(recentOTP)
+
+    if(recentOTP.length == 0){
+        return res.status(400).json({
+            success:false,
+            message : "OTP not found"
+        })
+    }else if(otp != recentOTP.otp){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid OTP"
+        })
+    }
+}
--- server/models/OTP.js
+++ server/models/OTP.js
@@ -18,7 +18,6 @@ const OTPSchema = new mongoose.Schema({
 });
 
 
-//a function -> to send emails
 async function sendVerificationEmail(email, otp) {
     try{
         const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
