d9e9e6f777646d8c1a95c04e3a00cd2d90fd379a
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2024-06-28 02:16:47 +0530

    controllers/ResetPassword completed model/User updated

--- server/models/User.js
+++ server/models/User.js
@@ -40,6 +40,12 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
+    token:{
+        type:String,
+    },
+    resetPassword:{
+        type:Date,
+    },
     courseProgress: [
         {
             type:mongoose.Schema.Types.ObjectId,
